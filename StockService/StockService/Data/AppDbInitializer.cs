4d864e8ec8a857f3ce8a1ac28ef7cff4a13c27f3
Sami Ellouze <sami.ellouze@ensi-uma.tn>
2023-07-29 14:08:53 +0100

    added just the file of the seeder

--- StockService/StockService/Data/AppDbInitializer.cs
+++ StockService/StockService/Data/AppDbInitializer.cs
@@ -1,6 +1,17 @@
-﻿namespace StockService.Data
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace StockService.Data
 {
     public class AppDbInitializer
     {
+        public static void Seed(IApplicationBuilder applicationBuilder)
+        {
+            using (var serviceScope = applicationBuilder.ApplicationServices.CreateScope())
+            {
+                var context = serviceScope.ServiceProvider.GetService<AppDbContext>();
+                //here you can seed your data. leave it to later
+
+            }
+        }
     }
 }
